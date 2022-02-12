5792085f0d086116781bbb0b158a4f771f2e1d2d
Janeth Mamani <janeth@ibm.com>
2022-02-12 20:12:34 -0300

    Formatação para imprimir váriaveis em python

--- IMC-BasicTraining/VariaveisIMC.py
+++ IMC-BasicTraining/VariaveisIMC.py
@@ -5,3 +5,6 @@ altura = float(input("Qual a sua altura? "))
 imc = peso / (altura**2) #Não precisava do parentesis pois a exponenciação tem a maior precedência aqui
 
 print(nome, 'tem', idade, 'de idade e seu IMC é', imc)
+print(f'{nome} tem {idade} anos de idade e seu imc é {imc:.2f}')
+print('{0} tem {2:.2f} de imc e {0} tem {1} de idade'.format(nome, idade, imc))
+print('{n} tem {im:.2f} de imc e {n} tem {i} de idade'.format(n=nome, i=idade, im=imc))
